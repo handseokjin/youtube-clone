a2a904887e0f1475c41b0323ad0c6d1a0fa6b44d
Seokjon Son <tnh3113@naver.com>
2023-08-02 15:28:56 +0900

    환경변수 설정 및 깃 페이지 배포

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,8 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     params: {
-        // api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
-        api_key: "7e2c5dcd90a7035bdd1cb321294639d5",
+        api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
         language: "ko-KR"
     }
 })
